eb5cde12578fbb9b8c23f66ce51b8594e33dc281
Jose Motta <josemotta@bampli.com>
2022-04-20 16:45:50 -0300

    criando componente link video

--- src/pages/Home/Banner/index.tsx
+++ src/pages/Home/Banner/index.tsx
@@ -10,6 +10,7 @@ import { makeStyles } from "@material-ui/styles";
 import { Theme } from "@material-ui/core";
 import BannerRating from "./BannerRating";
 import BannerCategory from "./BannerCategory";
+import VideoLink from "../../../components/Video/VideoLink";
 
 const useStyles = makeStyles((theme: Theme) => ({
     rootImage: {
@@ -96,6 +97,7 @@ const Banner: React.FunctionComponent = (props) => {
                     ))
                 }
             </Slider>
+            <VideoLink>Detalhes</VideoLink>
         </div>
     );
 }
